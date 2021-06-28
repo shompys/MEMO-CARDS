f65c7ab2593b60f5cc8e295d5177ba6a52c7f42a
jonathan <jonathangomezit@gmail.com>
2021-06-27 21:40:26 -0300

    se agrego funcionalidad de libreria react-card-flip

--- src/components/Grid/Card.js
+++ src/components/Grid/Card.js
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import ReactCardFlip from 'react-card-flip';
 
 const Card = ({id, name, image, handleClick, resetCard,reset, setReset}) => {
     
@@ -30,23 +31,30 @@ const Card = ({id, name, image, handleClick, resetCard,reset, setReset}) => {
     }, [reset, setReset])
     
     return(
-        <div className="card">
-         {!isCardFaceDown ?
-        
-            <div className="front" onClick={handle}>
+        <ReactCardFlip isFlipped={isCardFaceDown} flipDirection={'horizontal'} className="card">
 
-                <img src={image} alt={name}/>
-                <span className="name-card"> { name } </span>
+            <div className="card front" onClick={handle}>
+
+            <img src={image} alt={name}/>
+            <span className="name-card"> { name } </span>
 
             </div>
 
-            : 
+            <div className="card back" onClick={handle}>
 
-            <div className="back" onClick={handle}>
                 <img src="https://media.ambito.com/p/e36e8e53c7abba996531db19942b4527/adjuntos/239/imagenes/038/786/0038786610/rick-and-morty.jpg" alt="rick and morty"/>
+
             </div>
-        }
-        </div>
+
+        </ReactCardFlip>
+        // <div className="card">
+        //  {!isCardFaceDown ?
+
+            
+            
+            
+        // }
+        // </div>
 
     )
 }
